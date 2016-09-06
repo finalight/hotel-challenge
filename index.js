c9b1270d0e1c1077c71142f8e11aa646aac60b22
finalight <finalight@gmail.com>
2016-09-06 10:00:40 +0800

    split date array into 2D

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ function Hotel() {
     this.hotels = ['Lakewood', 'Bridgewood', 'Ridgewood'];
 }
 
-Hotel.prototype.findCheapest = function (input) {
-	//split the text
-	var input_array = input.split(":")
-	var customer_type = input_array[0].trim();
+Hotel.prototype.findCheapest = function(input) {
+    //split the text
+    var input_array = input.split(":")
+    var customer_type = input_array[0].trim();
 
-	console.log(customer_type);
+    var date_array = input_array[1].split(",").map(function(item) {
+        var trimmed_item = item.trim();
+        var opening_bracket_pos = trimmed_item.indexOf('(');
+        var closing_bracket_pos = trimmed_item.indexOf(')');
+        var day = trimmed_item.substr(opening_bracket_pos + 1, closing_bracket_pos - opening_bracket_pos - 1);
+        return [trimmed_item.trim(), day];
+    });
+
+
+    // console.log(date_array);
     return this.hotels[0];
 }
 
--- spec/test.spec.js
+++ spec/test.spec.js
@@ -2,7 +2,7 @@ var Hotel = require("../index")
 
 describe("Hotel room reservation test", function() {
     describe("input 16th to 18th march 2009", function() {
-        it("returns lakewood", function() {
+        xit("returns lakewood", function() {
             var hotel = new Hotel();
             var result = hotel.findCheapest('Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)');
             expect(result).toBe("Lakewood");
