545ed9a92e2734fa351c30970e7a0672e6797610
finalight <finalight@gmail.com>
2016-09-06 12:20:02 +0800

    refactor codes

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
+function HotelMetaData() {
+    return {
+        hotels: [{
+            name: 'Lakewood',
+            rating: 3,
+            pricing: { regular: { weekday: 110, weekend: 90 }, rewards: { weekday: 80, weekend: 80 } }
+        }, {
+            name: 'Bridgewood',
+            rating: 4,
+            pricing: { regular: { weekday: 160, weekend: 60 }, rewards: { weekday: 110, weekend: 50 } }
+        }, {
+            name: 'Ridgewood',
+            rating: 5,
+            pricing: { regular: { weekday: 220, weekend: 150 }, rewards: { weekday: 100, weekend: 40 } }
+        }],
+        weekdays: ['mon', 'tues', 'wed', 'thur', 'fri'],
+        weekends: ['sun','sat']
+    }
+}
+
+
 function Hotel() {
-    this.lakewood_rating = 3;
-    this.bridgewood_rating = 4;
-    this.ridgewood_rating = 5;
-    this.hotels = ['Lakewood', 'Bridgewood', 'Ridgewood'];
-    this.weekdays = ['mon', 'tue', 'wed', 'thur', 'fri'];
-    this.weekends = ['sat', 'sun'];
+}
+
+Hotel.getDates = function(dates) {
+    var hotelsInformation = HotelMetaData();
+    var result = dates.split(",").map(function(item) {
+        var trimmed_item = item.trim();
+        var opening_bracket_pos = trimmed_item.indexOf('(');
+        var closing_bracket_pos = trimmed_item.indexOf(')');
+        var date = trimmed_item.substr(0, opening_bracket_pos);
+        var day = trimmed_item.substr(opening_bracket_pos + 1, closing_bracket_pos - opening_bracket_pos - 1);
+        if (hotelsInformation.weekdays.indexOf(day) > -1) {
+            day = "weekday";
+        } else if (hotelsInformation.weekends.indexOf(day) > -1) {
+            day = "weekend";
+        }
+        return { date: trimmed_item.trim(), day_type: day };
+    });
+    return result;
 }
 
 Hotel.prototype.findCheapest = function(input) {
@@ -12,91 +45,36 @@ Hotel.prototype.findCheapest = function(input) {
     var input_array = input.split(":")
     var customer_type = input_array[0].toLowerCase().trim();
 
-    var date_array = input_array[1].split(",").map(function(item) {
-        var trimmed_item = item.trim();
-        var opening_bracket_pos = trimmed_item.indexOf('(');
-        var closing_bracket_pos = trimmed_item.indexOf(')');
-        var date = trimmed_item.substr(0, opening_bracket_pos);
-        var day = trimmed_item.substr(opening_bracket_pos + 1, closing_bracket_pos - opening_bracket_pos - 1);
-        return [trimmed_item.trim(), day];
-    });
+    var date_array = Hotel.getDates(input_array[1]);
 
-    lakewood_total = this.getHotelRate(this.hotels[0], customer_type, date_array);
-    bridgewood_total = this.getHotelRate(this.hotels[1], customer_type, date_array);
-    ridgewood_total = this.getHotelRate(this.hotels[2], customer_type, date_array);
-
-    console.log(lakewood_total);
-    console.log(bridgewood_total);
-    console.log(ridgewood_total);
-
-    //find the minimum of the three hotel pricing
-    if (lakewood_total > bridgewood_total) {
-        if (bridgewood_total > ridgewood_total) {
-            return this.hotels[2];
-        }
-        return this.hotels[1];
-    } else if (lakewood_total > ridgewood_total) {
-        if (ridgewood_total > bridgewood_total) {
-            return this.hotels[1];
-        }
-        return this.hotels[0];
-    } else if (lakewood_total === ridgewood_total) {
-        return this.hotels[2];
-    } else if (lakewood_total === bridgewood_total) {
-        return this.hotels[1];
-    } else if (bridgewood_total === ridgewood_total) {
-        return this.hotels[2];
-    }
-    return this.hotels[0]
+    return this.getHotelRate(customer_type, date_array).name;
 }
 
-Hotel.prototype.getHotelRate = function(hotel_name, customer_type, date_array) {
+Hotel.prototype.getHotelRate = function(customer_type, date_array) {
     //get total pricing for each hotel
-    var self = this;
+    var hotelsInformation = HotelMetaData();
     var total_pricing = 0;
-    var returnObject = {};
+    var hotel_result;
 
-    date_array.forEach(function(date) {
-        if (self.weekdays.indexOf(date[1]) > -1) {
-            if (customer_type === "regular") {
-                if (hotel_name.toLowerCase() === "lakewood") {
-                    total_pricing += 110;
-                } else if (hotel_name.toLowerCase() === "bridgewood") {
-                    total_pricing += 160;
-                } else if (hotel_name.toLowerCase() === "ridgewood") {
-                    total_pricing += 220;
-                }
-            } else if (customer_type === "rewards") {
-                if (hotel_name.toLowerCase() === "lakewood") {
-                    total_pricing += 80;
-                } else if (hotel_name.toLowerCase() === "bridgewood") {
-                    total_pricing += 110;
-                } else if (hotel_name.toLowerCase() === "ridgewood") {
-                    total_pricing += 100;
+    hotelsInformation.hotels.forEach(function(hotel) {
+        total_pricing = 0;
+
+        date_array.forEach(function(date) {
+            total_pricing += hotel.pricing[customer_type][date.day_type];
+        });
+        if (hotel_result == undefined) {
+            hotel_result = { name: hotel.name, price: total_pricing, rating: hotel.rating };
+        } else {
+            if (hotel_result.price > total_pricing) {
+                hotel_result = { name: hotel.name, price: total_pricing, rating: hotel.rating };
+            } else if (hotel_result.price === total_pricing) {
+                if (hotel_result.rating < hotel.rating) {
+                    hotel_result = { name: hotel.name, price: total_pricing, rating: hotel.rating };
                 }
             }
-
-        } else if (self.weekends.indexOf(date[1]) > -1) {
-            if (customer_type === "regular") {
-                if (hotel_name.toLowerCase() === "lakewood") {
-                    total_pricing += 90;
-                } else if (hotel_name.toLowerCase() === "bridgewood") {
-                    total_pricing += 60;
-                } else if (hotel_name.toLowerCase() === "ridgewood") {
-                    total_pricing += 150;
-                }
-            } else if (customer_type === "rewards") {
-                if (hotel_name.toLowerCase() === "lakewood") {
-                    total_pricing += 80;
-                } else if (hotel_name.toLowerCase() === "bridgewood") {
-                    total_pricing += 50;
-                } else if (hotel_name.toLowerCase() === "ridgewood") {
-                    total_pricing += 40;
-                }
-            }
-
         }
     });
-    return total_pricing;
+
+    return hotel_result;
 }
 module.exports = Hotel;
