bb863ce20b807c34f61b30747c95bdd06353753b
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2016-10-13 16:08:22 -0300

    Added getHotelRate() test.

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -33,3 +33,42 @@ describe("Hotel room reservation test", function() {
         });
     });
 });
+
+describe("Get best hotel based on price and rate (getHotelRate) test", function() {
+   describe("input client type regular, date 16th march 2009", function () {
+      it("returns Lakewood", function () {
+          var hotel = new Hotel();
+          var dates = [
+              { date: '16Mar2009(mon)', day_type: 'weekday' }
+          ];
+          var result = hotel.getHotelRate("regular", dates);
+          expect(result.name).toEqual('Lakewood');
+      }) ;
+   });   
+    
+    describe("input client type rewards, dates 19th to 21th march 2009", function () {
+      it("returns Ridgewood", function () {
+          var hotel = new Hotel();
+          var dates = [
+              { date: '19Mar2009(thur)', day_type: 'weekday' },
+              { date: '20Mar2009(fri)', day_type: 'weekday' },
+              { date: '21Mar2009(sat)', day_type: 'weekend'}
+          ];
+          var result = hotel.getHotelRate("rewards", dates);
+          expect(result.name).toEqual('Ridgewood');
+      });
+   });
+
+    describe("input client type regular, dates 20th to 22th march 2009", function () {
+        it("returns Bridgewood", function () {
+            var hotel = new Hotel();
+            var dates = [
+                { date: '20Mar2009(fri)', day_type: 'weekday'},
+                { date: '21Mar2009(sat)', day_type: 'weekend'},
+                { date: '22Mar2009(sun)', day_type: 'weekend'}
+            ];
+            var result = hotel.getHotelRate("regular", dates);
+            expect(result.name).toEqual('Bridgewood');
+        });
+    });
+});
